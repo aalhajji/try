895916e198f703b63258b022d513894b2c1d92c7
aalhajji@ryerson.ca <aalhajji@scherzo.ee.ryerson.ca>
2015-10-21 17:34:21 -0400

    new edit

---
+++ main.cpp
@@ -0,0 +1,60 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Oct 14, 2015
+ *      Author: aalhajji
+ */
+
+
+
+#include <iostream>
+#include <sstream>
+#include "header.hpp"
+using namespace std;
+
+int main() {
+
+	string Sentence = "Anna_Goldenberg system_biologist";
+	stringstream ss;
+	ss << Sentence;
+	string person, Expertise;
+	getline(ss,person,' ');
+	cout << "Who? " << person << endl;
+	ss >> Expertise;
+	cout << "Expertise? " << Expertise;
+
+}
+
+
+/*
+
+#define N_MOVIES 2
+
+struct movie_t {
+	char title [50];
+	int year;
+} films [N_MOVIES];
+
+void printmovie (movie_t movie);
+
+int main(){
+	char buffer [50];
+	int n;
+	for (n=0; n<N_MOVIES; n++){
+		cout << "Enter title: ";
+		cin.getline(films[n].title,50);
+		cout << "Enter year: ";
+		cin.getline(buffer,50);
+		stringstream(buffer)>>films[n].year;
+		//films[n].year = atoi (buffer);
+	}
+	cout << "\nYou have entered these movies:\n";
+	for (n=0; n<N_MOVIES; n++)
+		printmovie(films[n]);
+	return 0;
+}
+
+void printmovie (movie_t movie){
+	cout << movie.title;
+	cout << " (" << movie.year << ")\n";}
+*/
